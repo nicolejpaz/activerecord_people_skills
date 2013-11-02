95f5d910922b4df094384e4129c0c1fbeda7df7a
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-02 14:01:51 -0700

    screwed up, will try again later

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,4 @@
 class Skill < ActiveRecord::Base
+  has_many :users_skills
   has_many :users, through: :users_skills
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :users_skills
   has_many :skills, through: :users_skills
+  # validates :users, uniqueness: true
 end
